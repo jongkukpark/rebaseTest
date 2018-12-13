9cba6f4e8efa5f33916d84da39e869c14e9f102c
jongkukpark <jongkukpark162@cnu.ac.kr>
2018-12-13 23:45:19 +0900

    Main Conflict2

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
 public class Main {
+    public class Hi {
+
+    }
     public static void main(String[] args) {
         System.out.println("Hello Conflict1");
+        System.out.println("Hello Conflict2");
     }
 }
